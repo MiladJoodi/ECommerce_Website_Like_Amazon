91f3e7cc84781a227ea890fd01cc5b7af7aadad3
Milad Joodi <winbeta.com@gmail.com>
2024-03-10 12:45:26 +0330

    Update page.tsx

--- src/app/(front)/product/[slug]/page.tsx
+++ src/app/(front)/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import data from "@/lib/models/data";
+import Image from "next/image";
 import Link from "next/link";
 
 const ProductDetails = ({ params }: { params: { slug: string } }) => {
@@ -14,6 +15,29 @@ const ProductDetails = ({ params }: { params: { slug: string } }) => {
                 <Link href="/">back to products</Link>
             </div>
             <div className="grid md:grid-cols-4 md:gap-3">
+                <div className="md:col-span-2">
+                    <Image
+                        src={product.image}
+                        alt={product.name}
+                        width={600}
+                        height={640}
+                        sizes="100vw"
+                        style={{
+                            width: '100%',
+                            height: 'auto'
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <ul className="space-y-4">
+                        <li>
+                            <h1 className="text-xl">
+                                {product.name}
+                            </h1>
+                        </li>
+                    </ul>
+                </div>
 
             </div>
         </>
