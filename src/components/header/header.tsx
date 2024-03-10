9449ae1ea44afd493ffa02fd8289944196d3fb9b
Milad Joodi <winbeta.com@gmail.com>
2024-03-10 11:20:15 +0330

    update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,21 @@
+import Link from "next/link";
 
 const Header = () => {
     return (
         <header>
             <nav>
                 <div className="navbar justify-between bg-base-300">
-                    
+                    <Link href="/" className="btn btn-ghost textlg">
+                        Amazona ECommerce
+                    </Link>
+                    <ul className="flex">
+                        <li>
+                            <Link className="btn btn-ghost rounded-btn" href="/cart">Cart</Link>
+                        </li>
+                        <li>
+                            <Link className="btn btn-ghost rounded-btn" href="/signin">Signin</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
