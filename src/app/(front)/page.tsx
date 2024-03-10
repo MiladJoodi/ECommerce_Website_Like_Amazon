c2867670af25c551dbe9d83ae2e1222562b02743
Milad Joodi <winbeta.com@gmail.com>
2024-03-10 12:18:21 +0330

    import images

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,8 +1,12 @@
+import ProductItem from "@/components/products/ProductItem";
 
 export default function Home() {
   return (
     <>
       <h2 className="text-2xl py-2">Latest Products</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        <ProductItem />
+      </div>
     </>
   );
 }
